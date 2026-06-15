8fccefadcc1703c47f12c923e21bc1abb83ee0ec
fayaine <yuri007@hotmail.de>
2026-06-15 06:08:30 +0000

    Potential Icon fix

--- apps/client/src/app/model/lostark-task.ts
+++ apps/client/src/app/model/lostark-task.ts
@@ -2,7 +2,7 @@ import { TaskFrequency } from "./task-frequency";
 import { TaskScope } from "./task-scope";
 import { DataModel } from "../core/database/data-model";
 
-export const TASKS_VERSION = 54;
+export const TASKS_VERSION = 55;
 
 export interface LostarkTask extends DataModel {
   authorId?: string;
